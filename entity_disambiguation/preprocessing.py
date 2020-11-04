9e581c25ed614c1fe5bfc9937b348cbdb8ca944d
Will Seaton <41308207+wfseaton@users.noreply.github.com>
2020-11-03 19:14:31 -0500

    Integrate Wikipedia2vec pipeline work (#17)
    * Start wikipedia2vec pipeline; Update comments on basic_pipeline
    
    * Finalize wikipedia2vec pipeline; Update basic_pipeline to run over full_mention; Refresh EDA notebooks
    
    * Update notebooks with recent additions

--- entity_disambiguation/preprocessing.py
+++ entity_disambiguation/preprocessing.py
@@ -6,16 +6,20 @@ import pandas as pd
 
 
 def normalize_text(text):
-    """Return text after stripping external whitespace and lower casing."""
-    return str(text).strip().lower()
+    """Return turn in most comparative formatting
+    - Strip whitespace
+    - Lower case
+    - Spaces, not "_"
+    """
+    return str(text).strip().lower().replace("_", " ")
 
-
-def process_input(mydir, train=1.0):
+def process_input(mydir, match="token", train=1.0):
     """
     Function that processes the training input into required formatting (ACY dataset)
 
     Args:
     mydir, the directory that ADY dataset lives in on user's local machine
+    target, individual token or full_mention as classified by ACY
     train, percentage of training samples specified by user, default 80%
 
     Returns:
@@ -33,10 +37,15 @@ def process_input(mydir, train=1.0):
     new = ["token", "mention", "full_mention", "YAGO2", "wikipedia_URL", "wikipedia_ID", "freebase"]
     acy_df = acy_df.rename(columns=dict(zip(range(7), new)))
 
-    tokens = acy_df.token.values
+    # Decide what to input
+    if match == "token":
+        matches = acy_df.token.values
+    elif match == "full_mention":
+        matches = acy_df.full_mention.values
+
     ids = acy_df.wikipedia_ID.values
 
-    new_tokens = []
+    new_matches = []
     new_ids = []
     new_until_next = []
 
@@ -48,21 +57,21 @@ def process_input(mydir, train=1.0):
             # re-initialize word count in between
             until_next = 0
             new_ids.append(ids[i])
-            new_tokens.append(tokens[i].lower())
+            new_matches.append(matches[i])
         # else we append to words counts between mentions
         else:
             until_next += 1
 
     new_ids = np.array(new_ids)
-    train_len = int(train * len(new_tokens))
+    train_len = int(train * len(new_matches))
 
-    train_x = pd.DataFrame(columns=["token", "in_between_word_count"])
-    train_x["token"] = new_tokens[:train_len]
+    train_x = pd.DataFrame(columns=[match, "in_between_word_count"])
+    train_x[match] = new_matches[:train_len]
     train_x.in_between_word_count = new_until_next[:train_len]
     train_y = new_ids[:train_len]
 
-    test_x = pd.DataFrame(columns=["token", "in_between_word_count"])
-    test_x["token"] = new_tokens[train_len:]
+    test_x = pd.DataFrame(columns=[match, "in_between_word_count"])
+    test_x[match] = new_matches[train_len:]
     test_x.in_between_word_count = new_until_next[train_len:]
     test_y = new_ids[train_len:]
 
