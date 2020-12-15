61eb12a9b46e092fa1e4b9207f797ce5dd2f183a
Will Seaton <41308207+wfseaton@users.noreply.github.com>
2020-12-15 08:10:02 -0500

    Integrate refinement of embedding pipeline (#27)
    * Rename notebooks to reflect latest pipeline structure
    
    * Add varying vector dimensional experiments
    
    * Refresh anchor link pipeline with fixes
    
    * Add centroid v1
    
    * Refine Phase 1-3 notebooks
    
    * Finalize centroid implementation; Add with_priors flag to vector_similarity
    
    * Finalize embedding pipeline portions
    
    * Drop old unit test
    
    * Add base test
    
    Co-authored-by: Johannes Kolberg <johannes.kolberg@cognite.com>

--- tests/unit/test_preprocessing.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import pytest
-
-from entity_disambiguation.preprocessing import normalize_text
-
-
-def test_normer_works():
-    assert normalize_text("United States ") == "united states"
